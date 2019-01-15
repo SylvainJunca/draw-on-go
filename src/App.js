efbf5da47ebc0566dc1e2f4cf285245238e9085f
SylvainJunca <sylvain.junca@gmail.com>
2019-01-14 21:13:40 -0500

    installs the drawing canvas library

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome to Draw On GO !
           </p>
           <a
             className="App-link"
