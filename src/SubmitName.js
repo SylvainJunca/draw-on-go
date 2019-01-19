ab6f2985232c2855522d8cfc0a7594dfd8d9a784
SylvainJunca <sylvain.junca@gmail.com>
2019-01-19 15:35:33 -0500

    creates the SubmitName component

---
+++ src/SubmitName.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SubmitName extends Component {
+  
+  render() {
+
+    return (
+      <form onSubmit={this.props.submitName}>
+        <input type="text" name="room" placeholder="Please enter your username" maxLength="10"/>
+        <input type="submit"/>
+      </form>
+    )
+  }
+}
+
+export default SubmitName;
